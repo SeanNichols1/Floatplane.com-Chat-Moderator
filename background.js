5dba0c86ac60a558b8ea5a23ecee0fbf7ca98632
Sean Nichols <168885869+SeanNichols1@users.noreply.github.com>
2026-03-14 14:13:44 +1300

    Update background.js to only send tab activation messages to Floatplane.com tabs

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
-// Only send tab activation messages to Floatplane.com tabs
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function(tab) {
-        if (tab.url && tab.url.includes('floatplane.com')) {
-            // Logic to send tab activation message
-            console.log('Tab activated:', tab.url);
-        }
-    });
+// Only send tab activation messages to Floatplane.com tabs.
+
+const sendTabActivationMessage = (tab) => {
+    if (tab.url.includes('floatplane.com')) {
+        // Logic to send activation message
+        console.log('Sending activation message for Floatplane.com tab:', tab);
+    }
+};
+
+// Example: Listen for tab updates
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, sendTabActivationMessage);
 });
